9e35a903e625a8d64c21cb117eb601a0e0fcf211
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-07 19:05:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {Paramore: "Fake Happy"}
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign(playlist, {artistName: songTitle})
+function updatePlaylist(playlist, artistName, songTitle) { 
+  Object.assign(playlist, {artistName: songTitle})
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
