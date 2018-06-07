cebf011190031e58f77dfef294ed906015d8736f
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-07 18:59:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return Object.assign(playlist, {artistName: songTitle})
+  delete playlist.artistName
+  return playlist
 }
